43b218620c91cf86076f05d0a0af30dbe325f838
Cody Thompson <eldo.imo.rs@hotmail.com>
2020-06-13 21:59:36 -0700

    Begin basic instruction printing

--- src/instruction-processor.js
+++ src/instruction-processor.js
@@ -1,4 +1,5 @@
 const Printer = require('./util/printer');
+const _instructions = require('./util/instructions');
 
 const process = function(script) {
 
@@ -13,22 +14,69 @@ const process = function(script) {
     });
 
     let iStack = [];
-    let iStackIndex = 0;
 
     let sStack = [];
-    let sStackIndex = 0;
 
     let lStack = [];
-    let lStackIndex = 0;
 
     let index = 0;
     for(let index = 0; index < script.instructions.length; index++) {
         let instruction = script.instructions[index];
-        
+        let results;
+        let name;
+        let type;
+        let value;
+        let variable;
+        let variableIndex;
+        console.log(instruction);
+        switch(instruction.name) {
+            case 'PUSH_INT':
+            case 'PUSH_STRING':
+            case 'PUSH_LONG':
+                name = instruction.name;
+                type = name.split('_')[1].toLowerCase();
+                value = script.iValues[index];
+                results = this.asType('LITERAL')({
+                    name,
+                    type,
+                    value
+                });
+                if(type === 'int') iStack.unshift(results);
+                else if(type === 'string') sStack.unshift(results);
+                else if(type === 'long') lStack.unshift(results);
+                break;
+            case 'STORE_INT':
+            case 'STORE_STRING':
+            case 'STORE_LONG':
+                variableIndex = script.iValues[index];
+                variable = script.variables.find(e => e.index === variableIndex);
+                if(!variable) throw new Error('Unable to find variable with index: '+variableIndex);
+                name = instruction.name;
+                type = name.split('_')[1].toLowerCase();
+                if(type === 'int') value = iStack.shift();
+                else if(type === 'string') value = sStack.shift();
+                else if(type === 'long') value = lStack.shift();
+                results = this.asType('VARIABLE_ASSIGNATION')({
+                    name,
+                    type,
+                    variableIndex,
+                    variable,
+                    value
+                });
+                break;
+        }
+        if(iStack.length == 0 && sStack.length == 0 && lStack.length == 0 && results) {
+            printer.printInstruction(results);
+        }
     }
 
     console.log(printer.getData());
 
 }
+
+asType = (type) => (value) => ({
+    type,
+    value
+});
 
 module.exports = process;
--- src/util/printer.js
+++ src/util/printer.js
@@ -21,6 +21,30 @@ class Printer {
         this.data += data;
     }
 
+    printInstruction(results) {
+        console.log(results);
+        switch(results.type) {
+            case 'VARIABLE_ASSIGNATION':
+                this.print(results.value.variable.name+' = ');
+                this.printInstruction(results.value.value);
+                break;
+            case 'LITERAL':
+                let value = results.value.value;
+                switch(results.value.type) {
+                    case 'int':
+                        this.print(value);
+                        break;
+                    case 'string':
+                        this.print(`"${value}"`);
+                        break;
+                    case 'long':
+                        this.print(value+'L');
+                        break;
+                }
+                break;
+        }
+    }
+
     printScriptData(script) {
         this.print('//');
         this.print(script.name);
