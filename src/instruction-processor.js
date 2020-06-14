dd10e748dcc0d67ca5b71781448bb833d0e4125c
Cody Thompson <eldo.imo.rs@hotmail.com>
2020-06-13 23:30:04 -0700

    Allow basic script decoding with if statements

--- src/instruction-processor.js
+++ src/instruction-processor.js
@@ -19,15 +19,14 @@ const process = function(script) {
 
     let lStack = [];
 
-    let index = 0;
-    for(let index = 0; index < script.instructions.length; index++) {
-        let instruction = script.instructions[index];
+    const processInstruction = function(instruction, i) {
         let results;
         let name;
         let type;
         let value;
         let variable;
         let variableIndex;
+        let goto;
         console.log(instruction);
         switch(instruction.name) {
             case 'PUSH_INT':
@@ -35,27 +34,47 @@ const process = function(script) {
             case 'PUSH_LONG':
                 name = instruction.name;
                 type = name.split('_')[1].toLowerCase();
-                value = script.iValues[index];
+                if(type === 'int') value = script.iValues[i];
+                else if(type === 'string') value = script.sValues[i];
+                else if(type === 'long') value = script.lValues[i];
                 results = this.asType('LITERAL')({
                     name,
                     type,
                     value
                 });
-                if(type === 'int') iStack.unshift(results);
-                else if(type === 'string') sStack.unshift(results);
-                else if(type === 'long') lStack.unshift(results);
+                if(type === 'int') iStack.push(results);
+                else if(type === 'string') sStack.push(results);
+                else if(type === 'long') lStack.push(results);
                 break;
-            case 'STORE_INT':
-            case 'STORE_STRING':
-            case 'STORE_LONG':
-                variableIndex = script.iValues[index];
+            case 'LOAD_INT':
+            case 'LOAD_STRING':
+            case 'LOAD_LONG':
+                variableIndex = script.iValues[i];
                 variable = script.variables.find(e => e.index === variableIndex);
                 if(!variable) throw new Error('Unable to find variable with index: '+variableIndex);
                 name = instruction.name;
                 type = name.split('_')[1].toLowerCase();
-                if(type === 'int') value = iStack.shift();
-                else if(type === 'string') value = sStack.shift();
-                else if(type === 'long') value = lStack.shift();
+                results = this.asType('VARIABLE_LOAD')({
+                    name,
+                    type,
+                    variableIndex,
+                    variable
+                });
+                if(type === 'int') iStack.push(results);
+                else if(type === 'string') sStack.push(results);
+                else if(type === 'long') lStack.push(results);
+                break;
+            case 'STORE_INT':
+            case 'STORE_STRING':
+            case 'STORE_LONG':
+                variableIndex = script.iValues[i];
+                variable = script.variables.find(e => e.index === variableIndex);
+                if(!variable) throw new Error('Unable to find variable with index: '+variableIndex);
+                name = instruction.name;
+                type = name.split('_')[1].toLowerCase();
+                if(type === 'int') value = iStack.pop();
+                else if(type === 'string') value = sStack.pop();
+                else if(type === 'long') value = lStack.pop();
                 results = this.asType('VARIABLE_ASSIGNATION')({
                     name,
                     type,
@@ -64,10 +83,73 @@ const process = function(script) {
                     value
                 });
                 break;
+            case 'INT_LT':
+                let right = iStack.pop();
+                let left = iStack.pop();
+                goto = script.instructions[++i];
+                let gotoSize = script.iValues[i];
+                printer.print('if(');
+                printer.printInstruction(left);
+                printer.print(' < ');
+                printer.printInstruction(right);
+                printer.print(') {');
+                printer.newLine();
+                printer.tab();
+                for(let k = 0; k < gotoSize; k++) {
+                    let instr = script.instructions[++i];
+                    i = processInstruction(instr, i);
+                }
+                printer.untab();
+                printer.print('}');
+                printer.newLine();
+                break;
+            case 'RETURN':
+                let size = iStack.length + sStack.length + lStack.length;
+                if(size > 1) throw new Error('Only 1 value can be returned');
+                if(iStack.length > 0)
+                    value = iStack.pop();
+                else if(sStack.length > 0)
+                    value = sStack.pop();
+                else if(lStack.length > 0)
+                    value = lStack.pop();
+                else value = undefined;
+                results = this.asType('RETURN_STATEMENT')({
+                    value
+                });
+                break;
+            case 'SWITCH':
+                //TODO
+                //get case addresses from switchMap
+                //count down, include all instructions in scope
+                break;
+            default:
+                let params = [];
+                if(!instruction.popOrder) break;
+                for(let s of instruction.popOrder) {
+                    let result;
+                    if(s == 's') result = sStack.pop();
+                    else if(s == 'i') result = iStack.pop();
+                    else if(s == 'l') result = lStack.pop();
+                    params.push(result);
+                }
+                results = this.asType('FUNCTION_CALL')({
+                    name: instruction.name.toLowerCase(),
+                    params,
+                    returnType: instruction.returnType
+                });
+                if(instruction.returnType === 'void') break;
+                else if(instruction.returnType === 'int') iStack.push(results);
+                else if(instruction.returnType === 'string') sStack.push(results);
+                else if(instruction.returnType === 'long') lStack.push(results);
+                break;
         }
         if(iStack.length == 0 && sStack.length == 0 && lStack.length == 0 && results) {
             printer.printInstruction(results);
         }
+        return i;
+    }
+    for(let i = 0; i < script.instructions.length; i++) {
+        i = processInstruction(script.instructions[i], i);
     }
 
     console.log(printer.getData());
--- src/util/printer.js
+++ src/util/printer.js
@@ -13,34 +13,61 @@ class Printer {
         this.tabIndex++;
     }
 
-    unTab() {
+    untab() {
         this.tabIndex--;
     }
 
-    print(data) {
-        this.data += data;
+    print(data, tab=true) {
+        let s = '';
+        if(tab)
+            for(let i = 0; i < this.tabIndex; i++)
+                s += '\t';
+        s += data;
+        this.data += s;
     }
 
     printInstruction(results) {
-        console.log(results);
         switch(results.type) {
             case 'VARIABLE_ASSIGNATION':
                 this.print(results.value.variable.name+' = ');
                 this.printInstruction(results.value.value);
+                this.newLine();
+                break;
+            case 'VARIABLE_LOAD':
+                this.print(results.value.variable.name, false);
                 break;
             case 'LITERAL':
                 let value = results.value.value;
                 switch(results.value.type) {
                     case 'int':
-                        this.print(value);
+                        this.print(value, false);
                         break;
                     case 'string':
-                        this.print(`"${value}"`);
+                        this.print(`"${value}"`, false);
                         break;
                     case 'long':
-                        this.print(value+'L');
+                        this.print(value+'L', false);
                         break;
                 }
+                break;
+            case 'RETURN_STATEMENT':
+                this.print('return');
+                if(results.value.value) {
+                    this.print(' ', false);
+                    this.printInstruction(results.value.value);
+                }
+                this.newLine();
+                break;
+            case 'FUNCTION_CALL':
+                this.print(results.value.name+'(', results.value.returnType === 'void');
+                for(let i = 0; i < results.value.params.length; i++) {
+                    let param = results.value.params[i];
+                    this.printInstruction(param);
+                    if(i != results.value.params.length-1)
+                        this.print(', ', false);
+                }
+                this.print(')', false);
+                if(results.value.returnType === 'void') this.newLine();
                 break;
         }
     }
